e5c32e08770a59dd39bf728a0e7428be20e51bca
Mateus Varelo <mateusferreira703@gmail.com>
2021-05-16 12:37:28 -0300

    Implementei mais um teste com divisão.

--- Scripts_Python2021/cal_com_eval.py
+++ Scripts_Python2021/cal_com_eval.py
@@ -1,3 +1,4 @@
 def cal(num1,num2,operador):
     return eval(f'{num1}{operador}{num2}')
-print(cal(2,2.4,'**'))
+print(cal(2,3,'**'))# 2*2*2=8 (2 elevado a 3)
+print(cal(5,5,'/'))#5/5 = 1 (Divisão)
