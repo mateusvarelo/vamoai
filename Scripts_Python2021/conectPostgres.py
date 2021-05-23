894eaf1b5a0a8830138ed141f2cf7858b70ce291
Mateus Varelo <mateusferreira703@gmail.com>
2021-05-23 12:45:41 -0300

    Configurando conexão com db.

--- Scripts_Python2021/conectPostgres.py
+++ Scripts_Python2021/conectPostgres.py
@@ -1,7 +1,13 @@
 import psycopg2
 
 # Connect to your postgres DB
-conn = psycopg2.connect("dbname=postgres user=meubanco")
+conn = psycopg2.connect(
+    database ="" ,
+    user = "",
+    passworl = "",
+    host = "ec2-52-87-161-189.compute-1.amazonaws.com",
+    port = ""
+)
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
