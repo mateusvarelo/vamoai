1108d3abc84d6b390350f2aa1aae42d7a5dce09b
Mateus Varelo <mateusferreira703@gmail.com>
2021-05-23 14:58:31 -0300

    teste de conexão Db

--- Scripts_Python2021/conectPostgres.py
+++ Scripts_Python2021/conectPostgres.py
@@ -4,7 +4,7 @@ import psycopg2
 conn = psycopg2.connect(
     database ="grupo2" ,
     user = "grupo2",
-    passworl = "albaniatangerinajoaninha",
+    password = "albaniatangerinajoaninha",
     host = "ec2-52-87-161-189.compute-1.amazonaws.com",
     port = "5432"
 )
@@ -13,7 +13,7 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Execute a query
-cur.execute("SELECT * FROM grupo2")
+cur.execute("SELECT * FROM Steam")
 
 # Retrieve query results
 records = cur.fetchall()
