fb967b0337249f6a50981d6ed3138ae0f7efb069
Mateus Varelo <mateusferreira703@gmail.com>
2021-05-23 12:46:57 -0300

    port do Db

--- Scripts_Python2021/conectPostgres.py
+++ Scripts_Python2021/conectPostgres.py
@@ -2,11 +2,11 @@ import psycopg2
 
 # Connect to your postgres DB
 conn = psycopg2.connect(
-    database ="" ,
-    user = "",
+    database ="grupo2" ,
+    user = "grupo2",
     passworl = "",
     host = "ec2-52-87-161-189.compute-1.amazonaws.com",
-    port = ""
+    port = "5432"
 )
 
 # Open a cursor to perform database operations
