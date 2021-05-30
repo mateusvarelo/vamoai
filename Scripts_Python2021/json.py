657f772acbc68c0a1235a94aac2fbefaba5b1013
Mateus Varelo <mateusferreira703@gmail.com>
2021-05-30 11:10:03 -0300

    atualiza

--- Scripts_Python2021/json.py
+++ Scripts_Python2021/json.py
@@ -1,7 +1,7 @@
 import json
-import pprint
 content = {}
 
-with open('') as file_json:
-    content = json.load(file_json)
-pprint.pprint(content)    
+with open('MusicCollection.json') as file_json:
+    content = json.loads(file_json)
+
+print(content)   
