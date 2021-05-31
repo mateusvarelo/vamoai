207aecc86e256ed4c4a018d530b0d4f23d76cf90
Mateus Varelo <mateusferreira703@gmail.com>
2021-05-31 10:28:14 -0300

    sdsd

--- Scripts_Python2021/arquivos.py
+++ Scripts_Python2021/arquivos.py
@@ -1,1 +1,4 @@
-arquivo = open('meu.txt','w')
+arquivo = open('meu.txt','a')
+outro = open('txt.txt','r')
+# print(arquivo.readline(10))#apenas os 10 primeiros caracteres
+# print(arquivo.readlines())#ler todas as linhas
