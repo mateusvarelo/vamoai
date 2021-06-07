e216a8963cfd8d8325ba5db7abd5ea14ec8e155c
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-07 15:30:41 -0300

    apredendo a plotar graficos

---
+++ vamoai_modulo-4/matplot_statics.py
@@ -0,0 +1,25 @@
+import statistics as est
+import matplotlib.pyplot as plt
+
+lista_1 = [1,2,3,4,5,6,7,8,9,10]
+
+lista_2 = [11,12,13,14,15,16,17,18,19,20]
+
+#media 1
+# media_list_1 = print(est.mean(lista_1))
+# #media 2
+# media_list_2 = print(est.mean(lista_2))
+                     
+
+#moda 1
+# moda_list_1 = print(est.mode(lista_1))
+# moda 2
+# moda_list_2 = print(est.mode(lista_2))
+
+#mediana 1
+# median_list_1 = print(est.median(lista_1))
+# #mediana 2
+# median_list_2 = print(est.median(lista_2))
+
+plt.scatter(lista_1,lista_2)
+plt.show()
